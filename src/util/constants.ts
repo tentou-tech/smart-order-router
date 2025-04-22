037e825482a69830f72df103c56db9bd8784375a
duonghb53 <duonghb53@gmail.com>
2025-04-22 21:31:20 +0700

    add dex name

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -2,9 +2,21 @@ export const DEXES = {
   StoryHunt: {
     InitCodeHash:
       '0xd5178f9f07b08d01d075cc5b7e1a1ae23a37b3811522cb2fed1367201d51d4e5',
+    DexName: 'Storyhunt V3',
   },
   PiPerxV3: {
     InitCodeHash:
       '0xa8ffca5939bbe6e18e96df724ec3b3539269b282d1be4a535d654f640a37dcf5',
+    DexName: 'PiperX V3',
+  },
+  StoryHuntV2: {
+    InitCodeHash:
+      '0x0000000000000000000000000000000000000000000000000000000000000000',
+    DexName: 'Storyhunt V2',
+  },
+  PiperXV2: {
+    InitCodeHash:
+      '0x0000000000000000000000000000000000000000000000000000000000000000',
+    DexName: 'PiperX V2',
   },
 };
